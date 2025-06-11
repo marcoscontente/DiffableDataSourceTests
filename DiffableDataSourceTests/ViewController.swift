59a352a554c1f14058327064ab650a2dd4cfa6d1
Marcos Contente <contente.marcos@gmail.com>
2025-06-10 21:01:11 -0300

    Make simple TableView using DiffableDataSource

--- DiffableDataSourceTests/ViewController.swift
+++ DiffableDataSourceTests/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
+enum Section: Int, CaseIterable {
+    case fruits, vegetables
+
+    var title: String {
+        switch self {
+        case .fruits:
+            return "Fruits"
+        case .vegetables:
+            return "Vegeatables"
+        }
+    }
+}
+
+struct Item: Hashable {
+    let id = UUID()
+    let name: String
+    let category: Section
+}
+
 class ViewController: UIViewController {
+    private let allItems: [Item] = [
+        Item(name: "Apple", category: .fruits),
+        Item(name: "Banana", category: .fruits),
+        Item(name: "Orange", category: .fruits),
+        Item(name: "Carrot", category: .vegetables),
+        Item(name: "Green Peas", category: .vegetables)
+    ]
+
+    private lazy var tableView: UITableView = {
+        UITableView(frame: view.bounds)
+    }()
+
+    private var dataSource: UITableViewDiffableDataSource<Section, Item>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+
+        dataSource = UITableViewDiffableDataSource(tableView: tableView) { tableView, indexPath, item in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = item.name
+            return cell
+        }
+
+        tableView.dataSource = dataSource
+
+        applySnapshot()
     }
 
+    private func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections(Section.allCases)
 
+        snapshot.appendItems(allItems)
+
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
 }
-
